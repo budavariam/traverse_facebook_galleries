327b265ee2103bbcff410dfb7a6fa0ad627966f0
Mátyás Budavári <maty190@gmail.com>
2018-01-07 22:58:27 +0100

    Use main function

--- traverse_gallery.py
+++ traverse_gallery.py
@@ -5,19 +5,23 @@ import timeit
 from lib.files import Files
 from lib.gallery_crawler import GalleryCrawler
 
-if __name__ == "__main__":
-    print("[Facebook Gallery Downloader v0.3]")
-    START = timeit.default_timer()
+def main():
+    """ Traverse the galleries """
     try:
         with open(Files.OPTIONS.value) as options_file:
-            OPTIONS = json.load(options_file)
-            CREAWLER = GalleryCrawler(OPTIONS)
-            CREAWLER.run()
+            options = json.load(options_file)
+            creawler = GalleryCrawler(options)
+            creawler.run()
     except FileNotFoundError:
         print("[ ERROR: You should create your own {} from {}! ]".format(
             Files.OPTIONS.value,
             Files.OPTIONS_TEMPLATE.value
             ))
+
+if __name__ == "__main__":
+    print("[Facebook Gallery Downloader v0.3]")
+    START = timeit.default_timer()
+    main()
     STOP = timeit.default_timer()
     print("[ Time taken: %ss ]" % str(STOP - START))
     input("Press any key to continue...")
